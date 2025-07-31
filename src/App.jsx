8ee9605bef58cb72df240fcc020dcfc03064dab4
AlbericoJr <40373171+AlbericoJr@users.noreply.github.com>
2025-07-31 10:44:17 -0300

    Brainwave

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import { Benefits } from "./components/Benefits"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+
+export default function App() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Benefits />
+      </div>
+      <ButtonGradient />
+    </>
+  )
+}
+
+//https://youtu.be/B91wc5dCEBA?si=f5yan3MrFRsjBTEI 59:50
+
+//GitHub: https://github.com/adrianhajdin/brainwave/blob/main/src/components/Header.jsx
+
+// https://ui8-brainwave-landing.herokuapp.com/roadmap
---
+++ src/components/Notification.jsx
@@ -0,0 +1,33 @@
+import { notification1 } from "../assets"
+import { notificationImages } from "../constants"
+
+
+const Notification = ({tittle, className}) => {
+  return (
+    <div className={`${className || ""} flex items-center p-4 pr-6 bg-neutral-9/40 backdrop-blur border border-neutral-1/10 rounded-2xl gap-5`}>
+
+      <img src={notification1} width={62} height={62} className="rounded-xl" alt="image" />
+
+      <div className="flex-1">
+        <h6 className="mb-1 font-semibold text-base">{tittle}</h6>
+
+        <div className="flex items-center justify-between">
+          <ul className="flex -m-0.5">
+            {notificationImages.map((item, index) => (
+              <li key={index} className="flex w-6 h-6 border-2 border-neutral-12 rounded-full overflow-hidden">
+                <img src={item} className="w-full" width={20} height={20} alt={item} />
+              </li>
+            ))}
+          </ul>
+
+          <div className="body-2 text-neutral-13 text-right text-xs">
+            1 minuto atrás
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Notification
